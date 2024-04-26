6f52bfdf02fe36d1622865138070a4bba60a84fe
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-04-26 22:43:03 +0600

    Basic setup

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Essential theme supports
+ * */
+function theme_setup()
+{
+    /** automatic feed link*/
+    add_theme_support('automatic-feed-links');
+
+    /** tag-title **/
+    add_theme_support('title-tag');
+
+    /** post formats */
+    $post_formats = array('aside', 'image', 'gallery', 'video', 'audio', 'link', 'quote', 'status');
+    add_theme_support('post-formats', $post_formats);
+
+    /** post thumbnail **/
+    add_theme_support('post-thumbnails');
+
+    /** HTML5 support **/
+    add_theme_support('html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption'));
+
+    /** refresh widgest **/
+    add_theme_support('customize-selective-refresh-widgets');
+}
+add_action('after_setup_theme', 'theme_setup');
