f6c39b0ac97a62eac200fc6b061e76447d301772
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-04-29 01:39:09 +0600

    Added css and js file call

--- functions.php
+++ functions.php
@@ -25,3 +25,19 @@ function theme_setup()
     add_theme_support('customize-selective-refresh-widgets');
 }
 add_action('after_setup_theme', 'theme_setup');
+
+// clinic_add_theme_scripts
+function clinic_add_theme_scripts()
+{
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '1.1', 'all');
+    wp_enqueue_style('clinic-main', get_template_directory_uri() . '/assets/css/style.css', array(), '1.1', 'all');
+    wp_enqueue_style('style', get_stylesheet_uri());
+
+    wp_enqueue_script('clinic-main', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), 1.1, true);
+    wp_enqueue_script('owlcarousel', get_template_directory_uri() . '/assets/lib/owlcarousel/owl.carousel.min.js', array(), 1.1, true);
+
+    if (is_singular() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
+    }
+}
+add_action('wp_enqueue_scripts', 'clinic_add_theme_scripts');
