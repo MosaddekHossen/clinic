a08f888bdd146a1f415ed302f4b360b24e95496d
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-05-04 23:33:24 +0600

    Added banner

--- functions.php
+++ functions.php
@@ -36,8 +36,15 @@ function clinic_add_theme_scripts()
     wp_enqueue_style('clinic-main', get_template_directory_uri() . '/assets/css/style.css', array(), '1.1', 'all');
     wp_enqueue_style('style', get_stylesheet_uri());
 
+    wp_enqueue_script('wow', get_template_directory_uri() . '/assets/js/wow.min.js', array('jquery'), 1.1, true);
+    wp_enqueue_script('easing', get_template_directory_uri() . '/assets/js/easing.min.js', array('jquery'), 1.1, true);
+    wp_enqueue_script('waypoints', get_template_directory_uri() . '/assets/js/waypoints.min.js', array('jquery'), 1.1, true);
+    wp_enqueue_script('counterup', get_template_directory_uri() . '/assets/js/counterup.min.js', array('jquery'), 1.1, true);
+    wp_enqueue_script('carousel', get_template_directory_uri() . '/assets/js/owl.carousel.min.js', array('jquery'), 1.1, true);
+    wp_enqueue_script('moment', get_template_directory_uri() . '/assets/js/moment.min.js', array('jquery'), 1.1, true);
+    wp_enqueue_script('timezone', get_template_directory_uri() . '/assets/js/moment-timezone.min.js', array('jquery'), 1.1, true);
+    wp_enqueue_script('tempusdominus', get_template_directory_uri() . '/assets/js/tempusdominus-bootstrap-4.min.js', array('jquery'), 1.1, true);
     wp_enqueue_script('clinic-main', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), 1.1, true);
-    wp_enqueue_script('owlcarousel', get_template_directory_uri() . '/assets/lib/owlcarousel/owl.carousel.min.js', array(), 1.1, true);
 
     if (is_singular() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
